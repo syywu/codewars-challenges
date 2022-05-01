3b91ae36b0734d8704a5e735ae48c89d107d4fde
Samantha Wu <samanthayywu@gmail.com>
2022-05-01 15:35:49 +0100

    Convert a Boolean to a String

--- js/codewars.js
+++ js/codewars.js
@@ -200,3 +200,11 @@ function countSheeps(arrayOfSheep) {
   }
   return count;
 }
+
+// Convert a Boolean to a String
+
+/*
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+*/
