f5bbcebeac4f6b065da23a1018a78fd87a6f7f9a
Samantha Wu <samanthayywu@gmail.com>
2022-05-01 17:40:47 +0100

    Keep Hydrated solution

--- js/codewars.js
+++ js/codewars.js
@@ -256,3 +256,10 @@ time = 6.7---> litres = 3
 
 time = 11.8--> litres = 5
 */
+
+function litres(time) {
+  //   round down time to the smaller val
+   let res = time * 0.5; 
+    return Math.floor(res);
+  
+  }
