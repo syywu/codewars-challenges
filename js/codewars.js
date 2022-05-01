a7b2bf50fed138c2a57de1438f6364f38d8b3a12
Samantha Wu <samanthayywu@gmail.com>
2022-05-01 15:18:33 +0100

    Basic Mathematical Operations solution refactored

--- js/codewars.js
+++ js/codewars.js
@@ -141,3 +141,13 @@ function basicOp(operation, value1, value2)
         return value1 / value2
   }
 }
+
+function basicOp(operation, value1, value2)
+{
+  switch(operation){
+    case '+': return value1 + value2;
+    case '*': return value1 * value2;
+    case '-': return value1 - value2;
+    case '/': return value1 / value2;
+  }
+}
