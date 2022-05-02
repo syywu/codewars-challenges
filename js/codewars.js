b6e52e7870a99f693a256874b347d467a42f06dc
Samantha Wu <samanthayywu@gmail.com>
2022-05-02 15:59:20 +0100

    summation

--- js/codewars.js
+++ js/codewars.js
@@ -299,6 +299,20 @@ function positiveSum(arr){
   return addSum;
 }
 
+/*
+Summation
+
+Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+For example:
+
+summation(2) -> 3
+1 + 2
+
+summation(8) -> 36
+1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+*/
+
 /* Sentence Smash
 
 Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
@@ -307,6 +321,8 @@ Example
 
 ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
 */
+
+
 
 // String ends with?
 
