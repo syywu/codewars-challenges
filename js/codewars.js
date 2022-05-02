1f4fcd2e66d0dce92f8cf05ed48f80d4c5a6118b
Samantha Wu <samanthayywu@gmail.com>
2022-05-02 15:26:27 +0100

    Sum of positive

--- js/codewars.js
+++ js/codewars.js
@@ -278,6 +278,17 @@ function litres(time) {
     
   }
 
+// Sum of positive
+
+/* 
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+
+  */
+
 // String ends with?
 
 /*
