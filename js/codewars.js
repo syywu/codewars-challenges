23b5de21e69de62cacf0f0de07025d6ffbf36434
Samantha Wu <samanthayywu@gmail.com>
2022-05-02 18:05:59 +0100

    num to str

--- js/codewars.js
+++ js/codewars.js
@@ -336,6 +336,18 @@ var countSheep = function (num){
   return str;
 }
 
+/* num to str
+
+We need a function that can transform a number into a string.
+
+What ways of achieving this do you know?
+
+Examples:
+
+123 --> "123"
+999 --> "999"
+
+*/
 
 /* Sentence Smash
 
