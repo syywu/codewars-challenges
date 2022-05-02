b5c86cb4fb2bfb0978e65ace0eda32722286ace4
Samantha Wu <samanthayywu@gmail.com>
2022-05-02 15:32:11 +0100

    updated Sentence Smash

--- js/codewars.js
+++ js/codewars.js
@@ -299,14 +299,13 @@ function positiveSum(arr){
   return addSum;
 }
 
-/*
-Sentence Smash
+/* Sentence Smash
 
 Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
 
 Example
 
-['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is
+['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
 */
 
 // String ends with?
