151c8573273720cb302f6792c5d4984eac99e29b
Samantha Wu <samanthayywu@gmail.com>
2022-05-02 21:26:27 +0100

    Are You Playing Banjo

--- js/codewars.js
+++ js/codewars.js
@@ -393,7 +393,19 @@ function smash (words) {
   return str.trim(); 
 };
 
+/* Are You Playing Banjo?
 
+Create a function which answers the question "Are you playing banjo?".
+If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+The function takes a name as its only argument, and returns one of the following strings:
+
+name + " plays banjo" 
+name + " does not play banjo"
+Names given are always valid strings.
+
+
+*/
 
 // String ends with?
 
