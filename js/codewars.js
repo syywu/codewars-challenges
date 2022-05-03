446527e836edae61bac9f1015ee4d328f506b88d
Samantha Wu <samanthayywu@gmail.com>
2022-05-03 23:37:00 +0100

    School Paperwork solution

--- js/codewars.js
+++ js/codewars.js
@@ -464,7 +464,8 @@ function countBy(x, n) {
   return z;
 }
 
-/*
+/* School Paperwork
+
 Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
 
 Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
@@ -474,6 +475,15 @@ n= 5, m=5: 25
 n=-5, m=5:  0
 
 */
+
+function paperwork(n, m) {
+  if (n > 0 && m > 0){
+    return n * m 
+  }
+  return 0; 
+  
+}
+
 
 // String ends with?
 
