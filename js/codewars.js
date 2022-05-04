85ccd681dcdb4678119a89565e7dc957a7529f21
Samantha Wu <samanthayywu@gmail.com>
2022-05-04 10:53:06 +0100

    yes or no solution

--- js/codewars.js
+++ js/codewars.js
@@ -558,6 +558,13 @@ function grow(x){
 Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 */
 
+function boolToWord( bool ){
+  if(bool == true){
+    return "Yes"
+  }else{
+     return "No"
+  }
+}
 
 // String ends with?
 
