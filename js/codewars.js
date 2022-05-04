5ec43d3d99d6b6d8e3e375a4a05d197b3da30739
Samantha Wu <samanthayywu@gmail.com>
2022-05-04 10:13:40 +0100

    Clock

--- js/codewars.js
+++ js/codewars.js
@@ -484,7 +484,22 @@ function paperwork(n, m) {
   
 }
 
+/* Clock
 
+Your task is to write a function which returns the time since midnight in milliseconds.
+
+Example:
+h = 0
+m = 1
+s = 1
+
+result = 61000
+Input constraints:
+
+0 <= h <= 23
+0 <= m <= 59
+0 <= s <= 59
+ */
 // String ends with?
 
 /*
