6bb7ad6e089fa5f8f683d01c7aed5f4e8d9aac95
Samantha Wu <samanthayywu@gmail.com>
2022-05-04 10:52:43 +0100

    yes or no

--- js/codewars.js
+++ js/codewars.js
@@ -553,6 +553,12 @@ function grow(x){
   return res
 }
 
+/* yes or no
+
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+*/
+
+
 // String ends with?
 
 /*
