d49da53baab6c5a652fd31b540246c1791e2fe7e
Samantha Wu <samanthayywu@gmail.com>
2022-05-08 21:09:17 +0100

    Convert number to reversed array of digits

--- js/codewars.js
+++ js/codewars.js
@@ -576,3 +576,13 @@ Examples:
 solution('abc', 'bc') // returns true
 solution('abc', 'd') // returns false
   */
+
+/*
+Convert number to reversed array of digits
+
+Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+Example:
+348597 => [7,9,5,8,4,3]
+0 => [0]
+ */
