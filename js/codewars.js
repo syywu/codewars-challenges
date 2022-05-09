7b1fe31a926de434a8164ecb0e3da595b9f139ac
Samantha Wu <samanthayywu@gmail.com>
2022-05-09 17:19:21 +0100

    Double Char better solution

--- js/codewars.js
+++ js/codewars.js
@@ -667,3 +667,11 @@ function doubleChar(str) {
     }
     return newStr;
   }
+
+  function doubleChar(str) {
+    var word = '';
+    for (var i = 0; i < str.length; i++){
+      word = word + str[i] + str[i];
+    };
+    return word;
+  };
