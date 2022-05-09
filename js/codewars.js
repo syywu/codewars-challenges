fc2f9e1c24cb708bbab1464e0edc97a52efab7ff
Samantha Wu <samanthayywu@gmail.com>
2022-05-09 10:27:40 +0100

    Rock Paper Scissors solution

--- js/codewars.js
+++ js/codewars.js
@@ -601,7 +601,7 @@ function digitize(n) {
 
   /*
   Rock Paper Scissors
-  
+
 Let's play! You have to return which player won! In case of a draw return Draw!.
 
 Examples:
@@ -610,3 +610,13 @@ rps('scissors','paper') // Player 1 won!
 rps('scissors','rock') // Player 2 won!
 rps('paper','paper') // Draw!
   */
+
+const rps = (p1, p2) => {
+  if(p1 === "rock" && p2 === 'scissors' || p1 === 'scissors' && p2 === 'paper' || p1 === 'paper' && p2 === 'rock'){
+    return "Player 1 won!"
+  }
+  else if(p2 === "rock" && p1 === 'scissors' || p2 === 'scissors' && p1 === 'paper' || p2 === 'paper' && p1 === 'rock'){
+      return 'Player 2 won!'
+    }
+    return "Draw!"
+};
