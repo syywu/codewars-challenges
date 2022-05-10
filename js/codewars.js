ec3d5a9d96ead77ea1cf4659f0d474c23136576f
Samantha Wu <samanthayywu@gmail.com>
2022-05-10 10:28:16 +0100

    Make negative solution

--- js/codewars.js
+++ js/codewars.js
@@ -727,3 +727,10 @@ return newStr;
 // Notes
 // The number can be negative already, in which case no change is required.
 // Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+function makeNegative(num) {
+  if(num > 0){
+    return num * -1
+  }
+  return num
+}
