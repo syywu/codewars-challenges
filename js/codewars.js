8dbc59b47e7cb1a68af43864978d8ad81a5824db
Samantha Wu <samanthayywu@gmail.com>
2022-05-10 09:42:43 +0100

    DNA to RNA Conversion solution

--- js/codewars.js
+++ js/codewars.js
@@ -691,3 +691,15 @@ The input string can be of arbitrary length - in particular, it may be empty. Al
 
 
 */
+
+function DNAtoRNA(dna) {
+  // create a function which returns an RNA sequence from the given DNA sequence
+//   iterate through- pushing non 'T' to an array
+  let arr = dna.split('');
+  for (let i = 0; i<arr.length; i++){
+    if(arr[i] === "T"){
+      arr.splice(i, 1, "U");
+    }
+  }
+  return arr.join('');
+}
