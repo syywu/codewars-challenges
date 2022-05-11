847523184bc4e807712fdae2be38c5c89cd443a3
Samantha Wu <samanthayywu@gmail.com>
2022-05-11 10:05:17 +0100

    Count of positives / sum of negatives solution

--- js/codewars.js
+++ js/codewars.js
@@ -802,3 +802,22 @@ If the input is an empty array or is null, return an empty array.
 Example
 For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
  */
+
+function countPositivesSumNegatives(input) {
+
+  let arr = []
+  let count = 0
+  let sum= 0
+      if(input == null || input.length === 0){
+    return arr
+  }
+  for(let i=0; i<input.length; i++){
+     if(input[i] > 0){
+    count++
+  }else if(input[i] < 0){
+    sum+=input[i]
+  }
+  }
+   arr.push(count, sum)
+    return arr;
+}
