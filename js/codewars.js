3a6c0c8b3fcda149c52c558e5e771c48cc0ccfe9
Samantha Wu <samanthayywu@gmail.com>
2022-05-16 09:25:42 +0100

    vowel count solution

--- js/codewars.js
+++ js/codewars.js
@@ -908,3 +908,14 @@ The input string will only consist of lower case letters and/or spaces.
 
 
 */
+
+function getCount(str) {
+  let vowelsCount = 0;
+  for(let i = 0; i< str.length; i++){
+    if(str[i]== 'a' ||str[i]== 'e' || str[i]== 'i' ||str[i]== 'o' ||str[i]== 'u'){
+       vowelsCount++
+    }
+  }
+  
+  return vowelsCount;
+}
