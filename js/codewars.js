7b58be79a82d580dd3e3369f13d4adc024dfdc56
Samantha Wu <samanthayywu@gmail.com>
2022-05-16 09:25:21 +0100

    vowel count

--- js/codewars.js
+++ js/codewars.js
@@ -897,3 +897,14 @@ function minMax(arr){
   arr1.push(arr[0], arr[arr.length -1])
   return arr1; 
 }
+
+/* vowel count 
+
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.
+
+
+*/
