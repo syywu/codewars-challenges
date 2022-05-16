a492cec3381701d336269e79b52093a4d5783d60
Samantha Wu <samanthayywu@gmail.com>
2022-05-16 09:30:16 +0100

    volume of cuboid

--- js/codewars.js
+++ js/codewars.js
@@ -933,3 +933,9 @@ function getCount(str) {
   
   return vowelsCount;
 }
+
+/* volume of cuboid
+
+Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+
+ */
