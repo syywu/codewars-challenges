2583eec03d9042aecb2bfb66b05baab003b5921a
Samantha Wu <samanthayywu@gmail.com>
2022-05-18 09:44:49 +0100

    get grad solution

--- js/codewars.js
+++ js/codewars.js
@@ -998,3 +998,20 @@ function highAndLow(numbers) {
 
   return newArr.reverse().join(" ");
 }
+
+function getGrade(s1, s2, s3) {
+  //  add 3 paras and divide by 3
+  //   switch statement
+  let score = (s1 + s2 + s3) / 3;
+  if (score >= 90) {
+    return "A";
+  } else if (score >= 80) {
+    return "B";
+  } else if (score >= 70) {
+    return "C";
+  } else if (score >= 60) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
