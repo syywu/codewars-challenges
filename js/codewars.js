b0ca7b6097cf377eb0dda28250d947f8dfa6f05d
Samantha Wu <samanthayywu@gmail.com>
2022-05-23 09:47:27 +0100

    reverse the word

--- js/codewars.js
+++ js/codewars.js
@@ -1157,3 +1157,10 @@ function dontGiveMeFive(start, end) {
   }
   return count;
 }
+
+// reverse the word
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
