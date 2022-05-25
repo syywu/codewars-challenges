d9503f94e1306e538c1d81788fa53b594966c757
Samantha Wu <samanthayywu@gmail.com>
2022-05-25 12:51:54 +0100

    BINGO or not solution 2

--- js/codewars.js
+++ js/codewars.js
@@ -1313,3 +1313,13 @@ function bingo(a) {
   }
   return "LOSE";
 }
+
+function bingo(a) {
+  let n = [2, 9, 14, 7, 15];
+  for (let i = 0; i < n.length; i++) {
+    if (!a.includes(n[i])) {
+      return "LOSE";
+    }
+  }
+  return "WIN";
+}
