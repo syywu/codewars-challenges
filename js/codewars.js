a71a8ee38e24342080e9401d661c205861cecbf9
Samantha Wu <samanthayywu@gmail.com>
2022-05-30 09:13:54 +0100

    square sum

--- js/codewars.js
+++ js/codewars.js
@@ -1349,3 +1349,10 @@ function enough(cap, on, wait) {
 }
 
 var enough = (cap, on, wait) => (cap >= on + wait ? 0 : on + wait - cap);
+
+/* square sum
+
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+ */
