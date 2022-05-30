f54221d9f50d5c3136cf6978a802b1dd71cde771
Samantha Wu <samanthayywu@gmail.com>
2022-05-30 23:02:33 +0100

    maps.js

---
+++ js/arrayDiff.js
@@ -0,0 +1,41 @@
+/*
+Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+It should remove all values from list a, which are present in list b keeping their order.
+
+arrayDiff([1,2],[1]) == [2]
+If a value is present in b, all of its occurrences must be removed from the other:
+
+arrayDiff([1,2,2,2,3],[2]) == [1,3]
+
+*/
+function arrayDiff(a, b) {
+  // Temporary array, containing all values from a, which are not contained in b
+  let diffArray = [];
+  // Looping over a
+  for (let i = 0; i < a.length; i++) {
+    // Per default we say that b does not contain a
+    let bContainsAValue = false;
+    // Loop over b
+    for (let y = 0; y < b.length; y++) {
+      // If any value in b is the same as the current value from a, we set bContainsAValue to true
+      if (a[i] === b[y]) bContainsAValue = true;
+    }
+    // Now only if bContainsAValue is still false, meaning it does not contain the a value, we push this value into our temporary array
+    if (!bContainsAValue) diffArray.push(a[i]);
+  }
+  // In the end we return the temporary array
+  return diffArray;
+}
+
+function arrayDiff(a, b) {
+  let newArr = [];
+  for (let i = 0; i < a.length; i++) {
+    let bValInA = false;
+    for (let j = 0; j < b.length; j++) {
+      if (a[i] === b[j]) bValInA = true;
+    }
+    if (!bValInA) newArr.push(a[i]);
+  }
+  return newArr;
+}
---
+++ js/maps.js
@@ -0,0 +1,17 @@
+/* Lost without map
+
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+
+*/
+
+function maps(x) {
+  let newArr = [];
+  for (let i = 0; i < x.length; i++) {
+    newArr.push(x[i] * 2);
+  }
+  return newArr;
+}
