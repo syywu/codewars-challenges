e4e540f9668954388e4a40103c60d55e30649f18
Samantha Wu <samanthayywu@gmail.com>
2022-05-30 23:00:36 +0100

    numberToString.js

---
+++ numberToString.js
@@ -0,0 +1,16 @@
+/* num to str
+
+We need a function that can transform a number into a string.
+
+What ways of achieving this do you know?
+
+Examples:
+
+123 --> "123"
+999 --> "999"
+
+*/
+
+function numberToString(num) {
+  return num.toString();
+}
