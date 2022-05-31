df1dfb2771bfbda02bead704135828b50259ba63
Samantha Wu <samanthayywu@gmail.com>
2022-05-31 09:27:06 +0100

    grow.js

---
+++ js/grow.js
@@ -0,0 +1,14 @@
+/*  Reduce but Grow
+
+Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+*/
+
+function grow(x) {
+  let res = 1;
+  for (let i = 0; i < x.length; i++) {
+    res *= x[i];
+  }
+  return res;
+}
