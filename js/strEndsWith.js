b81ee10c5700caa38c0384078820a20f0ae75ea4
Samantha Wu <samanthayywu@gmail.com>
2022-05-31 20:34:19 +0100

    strEndsWith

---
+++ js/strEndsWith.js
@@ -0,0 +1,20 @@
+// String ends with?
+
+/*
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+  */
+
+/*
+Convert number to reversed array of digits
+
+Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+Example:
+348597 => [7,9,5,8,4,3]
+0 => [0]
+ */
