93249085561ee0d890fe7d5b06588a70f0a8b577
Samantha Wu <samanthayywu@gmail.com>
2022-06-06 09:51:32 +0100

    doubleChar solution

--- js/doubleChar.js
+++ js/doubleChar.js
@@ -8,3 +8,16 @@ Examples (Input -> Output):
 * "1234!_ "     -> "11223344!!__  "
   
 */
+
+function doubleChar(str) {
+  //   split str into individual letters
+  //   new str
+  //   iterate usinf for loop
+  //   push i twice to str
+  let arr = str.split("");
+  let newStr = "";
+  for (let i = 0; i < arr.length; i++) {
+    newStr += arr[i].repeat(2).toString();
+  }
+  return newStr;
+}
