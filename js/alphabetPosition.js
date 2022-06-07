4c0c7f08ffce48805cf059769bd2c68f87154ef9
Samantha Wu <samanthayywu@gmail.com>
2022-06-07 22:27:30 +0100

    alphabetPosition solution

--- js/alphabetPosition.js
+++ js/alphabetPosition.js
@@ -10,3 +10,20 @@ Example
 alphabetPosition("The sunset sets at twelve o' clock.")
 Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
  */
+
+function alphabetPosition(text) {
+  //   str to arr
+  //   change all to lowercase
+  //   only return when arr[i] === [a-z]gi
+  //   assign all in an obj?
+  text = text.toLowerCase();
+  let arr = [];
+  for (let i = 0; i < text.length; i++) {
+    let num = text.charCodeAt(i);
+    if (num <= 122 && num >= 97) {
+      arr.push(num - 96);
+    }
+  }
+
+  return arr.join(" ");
+}
