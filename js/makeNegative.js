e3cb328e88c8ab4724634880afea6968c171ab99
Samantha Wu <samanthayywu@gmail.com>
2022-06-08 10:17:11 +0100

    makeNegative solution

--- js/makeNegative.js
+++ js/makeNegative.js
@@ -9,3 +9,10 @@
 // Notes
 // The number can be negative already, in which case no change is required.
 // Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+function makeNegative(num) {
+  if (num > 0) {
+    return num * -1;
+  }
+  return num;
+}
