8722e08e0ffffec1ae0d89110c7ddcbfdcc1cef8
Samantha Wu <samanthayywu@gmail.com>
2022-06-08 18:58:40 +0100

    accum solution

--- js/accum.js
+++ js/accum.js
@@ -7,3 +7,18 @@ accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 accum("cwAt") -> "C-Ww-Aaa-Tttt"
 The parameter of accum is a string which includes only letters from a..z and A..Z. 
 */
+
+function accum(s) {
+  // turn str into arr
+  //   initiate new str
+  //   arr[i]+1
+  //   - = arr.length -1. add it to new str
+  //   change all first char to caps
+  //   join all with -
+  s = s.toLowerCase();
+  let str = "";
+  for (let i = 0; i < s.length; i++) {
+    str += s[i].toUpperCase() + s[i].repeat(i) + "-";
+  }
+  return str.slice(0, -1);
+}
