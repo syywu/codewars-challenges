09b45ef627bc84db2aea0dc9f4e6b1b4059e9179
Samantha Wu <samanthayywu@gmail.com>
2022-06-14 19:18:08 +0100

    howMuchILoveYou solution

--- js/howMuchILoveYou.js
+++ js/howMuchILoveYou.js
@@ -11,3 +11,21 @@ When the last petal was torn there were cries of excitement, dreams, surging tho
 
 Your goal in this kata is to determine which phrase the girls would say for a flower of a given number of petals, where nb_petals > 0.
 */
+
+function howMuchILoveYou(nbPetals) {
+  // phrases loops through over and over again
+  //   return the phrase
+  //   * 6
+  let answers = [
+    "I love you",
+    "a little",
+    "a lot",
+    "passionately",
+    "madly",
+    "not at all",
+  ];
+  if (nbPetals % answers.length === 0) {
+    return answers[answers.length - 1];
+  }
+  return answers[(nbPetals % answers.length) - 1];
+}
