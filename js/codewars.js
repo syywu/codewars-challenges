0f51c5e5f80a05c058ef3087651fc9ff3312c587
Samantha Wu <samanthayywu@gmail.com>
2022-06-14 19:17:34 +0100

    howMuchILoveYou

--- js/codewars.js
+++ js/codewars.js
@@ -10,50 +10,13 @@ function digitize(n) {
   return arr;
 }
 
-// repeat str
-// repeat s by n times
 
-function repeatStr(n, s) {
-  return s.repeat(n);
-}
 
-function repeatStr(n, s) {
-  let str = "";
-  for (let i = 0; i < n; i++) {
-    str += s;
-  }
-  return str;
-}
 
-/* Count of positives / sum of negatives
 
-Given an array of integers.
 
-Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
 
-If the input is an empty array or is null, return an empty array.
 
-Example
-For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
- */
-
-function countPositivesSumNegatives(input) {
-  let arr = [];
-  let count = 0;
-  let sum = 0;
-  if (input == null || input.length === 0) {
-    return arr;
-  }
-  for (let i = 0; i < input.length; i++) {
-    if (input[i] > 0) {
-      count++;
-    } else if (input[i] < 0) {
-      sum += input[i];
-    }
-  }
-  arr.push(count, sum);
-  return arr;
-}
 
 /* Reversed sequence
 
