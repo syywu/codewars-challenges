a7509710a9ddb747a80ba849435ec6ae058c1e31
Samantha Wu <samanthayywu@gmail.com>
2022-06-15 10:28:32 +0100

    isTriangle solution

--- js/isTriangle.js
+++ js/isTriangle.js
@@ -4,3 +4,10 @@ Implement a function that accepts 3 integer values a, b, c. The function should 
 (In this case, all triangles must have surface greater than 0 to be accepted).
 
 */
+
+function isTriangle(a, b, c) {
+  if (a + c > b && a + b > c && b + c > a) {
+    return true;
+  }
+  return false;
+}
