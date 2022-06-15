5e6f51a427260ce9ececda2e3ede34a9a93d7f12
Samantha Wu <samanthayywu@gmail.com>
2022-06-15 21:26:05 +0100

    robberEncode solution

--- js/robberEncode.js
+++ js/robberEncode.js
@@ -9,3 +9,31 @@ In this example, the consonants f, b and r are modified. The vowels o and a are 
 'foo bar' => 'fofoo bobaror' (`fof-o-o bob-a-ror`)
 Note: The dashes - in the example are added for readability and should not be included in the output.
 */
+
+function robberEncode(sentence) {
+  // yoyouror cocodode hoherore
+  let str = "";
+  for (let i = 0; i < sentence.length; i++) {
+    if (isConsonant(sentence[i])) {
+      if (sentence[i] === sentence[i].toUpperCase()) {
+        str += sentence[i] + "O" + sentence[i];
+      } else {
+        str += sentence[i] + "o" + sentence[i];
+      }
+    } else {
+      str += sentence[i];
+    }
+  }
+  return str;
+}
+
+function isConsonant(char) {
+  char = char.toUpperCase();
+  let cons = "BCDFGHJKLMNPQRSTVWXYZ";
+  for (let i = 0; i < cons.length; i++) {
+    if (char === cons[i]) {
+      return true;
+    }
+  }
+  return false;
+}
