0b3dc46cc9a9626e158e646f134008ce45ddd72e
Samantha Wu <samanthayywu@gmail.com>
2022-06-15 21:25:31 +0100

    robberEncode

---
+++ js/robberEncode.js
@@ -0,0 +1,11 @@
+/*
+The Robber Language (Rövarspråket) is a Swedish cant used by children to mislead people not familiar with the language. It's used by kids in several screenplays created by Astrid Lindgren.
+
+The idea is that every consonant in a sentence is duplicated, with an o inserted in between (b becomes bob). Vowels are left untouched (a remains an a). It's quite hard for an untrained ear to understand a conversation encoded in this manner, especially if spoken rapidly.
+
+Example
+In this example, the consonants f, b and r are modified. The vowels o and a are left untouched.
+
+'foo bar' => 'fofoo bobaror' (`fof-o-o bob-a-ror`)
+Note: The dashes - in the example are added for readability and should not be included in the output.
+*/
