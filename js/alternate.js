8adcaec180d36c9fd0b9ee0ef76c21ce03d5dcd3
Samantha Wu <samanthayywu@gmail.com>
2022-06-15 20:36:19 +0100

    alternate

---
+++ js/alternate.js
@@ -0,0 +1,6 @@
+/*
+Given a number n and two values firstValue and secondValue, build an array of size n filled with firstValue and secondValue alternating.
+
+for input:                5, true, false
+expected result would be: [true, false, true, false, true]
+*/
