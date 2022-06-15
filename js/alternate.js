1a8d0d0633f5079ae1bb6fa78100a9e76315603a
Samantha Wu <samanthayywu@gmail.com>
2022-06-15 20:36:43 +0100

    alternate solution

--- js/alternate.js
+++ js/alternate.js
@@ -4,3 +4,18 @@ Given a number n and two values firstValue and secondValue, build an array of si
 for input:                5, true, false
 expected result would be: [true, false, true, false, true]
 */
+
+function alternate(n, firstValue, secondValue) {
+  // your code
+  //   for loop
+  //   return an arr
+  let arr = [];
+  for (let i = 0; i < n; i++) {
+    if (i % 2 === 0) {
+      arr.push(firstValue);
+    } else {
+      arr.push(secondValue);
+    }
+  }
+  return arr;
+}
