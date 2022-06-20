44d5c5afa774036158fc0a425326899f940f19c8
Samantha Wu <samanthayywu@gmail.com>
2022-06-20 12:13:59 +0100

    areaOrPerimeter

--- js/codewars.js
+++ js/codewars.js
@@ -18,20 +18,8 @@ function digitize(n) {
 
 
 
-/* Reversed sequence
 
-Build a function that returns an array of integers from n to 1 where n>0.
 
-Example : n=5 --> [5,4,3,2,1]
- */
-
-const reverseSeq = (n) => {
-  let arr = [];
-  for (let i = n; i > 0; i--) {
-    arr.push(i);
-  }
-  return arr;
-};
 
 /* Area or Perimeter
 You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
