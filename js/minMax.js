cead3f0b7471a2b4c1e084a6bd49af03ce703153
Samantha Wu <samanthayywu@gmail.com>
2022-06-24 16:54:55 +0100

    minMax solution

--- js/minMax.js
+++ js/minMax.js
@@ -12,3 +12,10 @@ minMax([1])           == [1, 1]
 Remarks
 All arrays or lists will always have at least one element, so you don't need to check the length. Also, your function will always get an array or a list, you don't have to check for null, undefined or similar.
 */
+
+function minMax(arr) {
+  arr.sort((a, b) => a - b);
+  let arr1 = [];
+  arr1.push(arr[0], arr[arr.length - 1]);
+  return arr1;
+}
