5774efa1c4ef7db307b1a8d3481d5e8b103d0731
Samantha Wu <samanthayywu@gmail.com>
2022-06-24 18:38:20 +0100

    consecutive

--- js/codewars.js
+++ js/codewars.js
@@ -10,45 +10,6 @@ function digitize(n) {
   return arr;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* The highest profit wins
-
-Ben has a very simple idea to make some profit: he buys something and sells it again. Of course, this wouldn't give him any profit at all if he was simply to buy and sell it at the same price. Instead, he's going to buy it for the lowest possible price and sell it at the highest.
-
-Task
-Write a function that returns both the minimum and maximum number of the given list/array.
-
-Examples
-minMax([1,2,3,4,5])   == [1,5]
-minMax([2334454,5])   == [5, 2334454]
-minMax([1])           == [1, 1]
-Remarks
-All arrays or lists will always have at least one element, so you don't need to check the length. Also, your function will always get an array or a list, you don't have to check for null, undefined or similar.
-*/
-
-function minMax(arr) {
-  arr.sort((a, b) => a - b);
-  let arr1 = [];
-  arr1.push(arr[0], arr[arr.length - 1]);
-  return arr1;
-}
-
 /* vowel count 
 
 Return the number (count) of vowels in the given string.
@@ -486,3 +447,7 @@ function squareSum(numbers) {
   }
   return sum;
 }
+
+// You are given a list of unique integers arr, and two integers a and b. Your task is to find out whether or not a and b appear consecutively in arr, and return a boolean value (True if a and b are consecutive, False otherwise).
+
+// It is guaranteed that a and b are both present in arr.
