a1559ad2bd78d24819ca4c2ec8123b944e3cd7ef
Samantha Wu <samanthayywu@gmail.com>
2022-06-25 13:10:39 +0100

    getCount solution

--- js/getCount.js
+++ js/getCount.js
@@ -6,5 +6,21 @@ We will consider a, e, i, o, u as vowels for this Kata (but not y).
 
 The input string will only consist of lower case letters and/or spaces.
 
-
 */
+
+function getCount(str) {
+  let vowelsCount = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str[i] == "a" ||
+      str[i] == "e" ||
+      str[i] == "i" ||
+      str[i] == "o" ||
+      str[i] == "u"
+    ) {
+      vowelsCount++;
+    }
+  }
+
+  return vowelsCount;
+}
