be4593411405731d6f9034ff86d94899e76d581f
Samantha Wu <samanthayywu@gmail.com>
2022-07-07 16:17:23 +0100

    getVolumeOfCuboid

--- js/codewars.js
+++ js/codewars.js
@@ -10,48 +10,6 @@ function digitize(n) {
   return arr;
 }
 
-/* vowel count 
-
-Return the number (count) of vowels in the given string.
-
-We will consider a, e, i, o, u as vowels for this Kata (but not y).
-
-The input string will only consist of lower case letters and/or spaces.
-
-
-*/
-
-function getCount(str) {
-  let vowelsCount = 0;
-  for (let i = 0; i < str.length; i++) {
-    if (
-      str[i] == "a" ||
-      str[i] == "e" ||
-      str[i] == "i" ||
-      str[i] == "o" ||
-      str[i] == "u"
-    ) {
-      vowelsCount++;
-    }
-  }
-
-  return vowelsCount;
-}
-
-function getCount(str) {
-  var vowelsCount = 0;
-  var vowels = ["a", "e", "i", "o", "u"];
-  for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < vowels.length; j++) {
-      if (str[i] === vowels[j]) {
-        vowelsCount++;
-      }
-    }
-  }
-
-  return vowelsCount;
-}
-
 /* volume of cuboid
 
 Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
