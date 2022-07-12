9feba4c32a2d1bf6d8eaa1acd7be857f6d9917ac
Samantha Wu <samanthayywu@gmail.com>
2022-07-12 10:06:03 +0100

    getDivisorsCount solution

--- js/getDivisorsCount.js
+++ js/getDivisorsCount.js
@@ -9,3 +9,13 @@ Examples (input --> output)
 12 --> 6 (1, 2, 3, 4, 6, 12)
 30 --> 8 (1, 2, 3, 5, 6, 10, 15, 30)
 */
+
+function getDivisorsCnt(n) {
+  let count = 0;
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 0) {
+      count++;
+    }
+  }
+  return count;
+}
