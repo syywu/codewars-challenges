acf12960eeadc828390210c7ce6b4dbbd92e4389
Samantha Wu <samanthayywu@gmail.com>
2022-07-20 16:44:03 +0100

    getGrade soltuion

--- js/getGrade.js
+++ js/getGrade.js
@@ -11,3 +11,20 @@ Numerical Score	Letter Grade
 Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
 
 */
+
+function getGrade(s1, s2, s3) {
+  //  add 3 paras and divide by 3
+  //   switch statement
+  let score = (s1 + s2 + s3) / 3;
+  if (score >= 90) {
+    return "A";
+  } else if (score >= 80) {
+    return "B";
+  } else if (score >= 70) {
+    return "C";
+  } else if (score >= 60) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
