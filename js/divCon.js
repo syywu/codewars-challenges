6e0e5b8457780c3361dd4db3fb90f30acdf39e88
Samantha Wu <samanthayywu@gmail.com>
2022-07-23 19:38:59 +0100

    divCon solution

--- js/divCon.js
+++ js/divCon.js
@@ -3,3 +3,22 @@ Given a mixed array of number and string representations of integers, add up the
 
 Return as a number.
 */
+
+function divCon(x) {
+  //   add strings int
+  //   sub int
+  //   filter array into arrays
+  let int = x.filter((i) => typeof i === "number");
+  console.log(int);
+  let str = x.filter((i) => typeof i === "string");
+  console.log(str);
+  let sum = 0;
+  let sub = 0;
+  for (let i = 0; i < str.length; i++) {
+    sum += Number(str[i]);
+  }
+  for (let i = 0; i < int.length; i++) {
+    sub += int[i];
+  }
+  return sub - sum;
+}
